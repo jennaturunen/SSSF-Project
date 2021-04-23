69529148579c1048dde55f3647e3142d33f0b20c
jennaturunen <jenna.turunen2@gmail.com>
2021-04-23 21:12:17 +0300

    added account type to register form

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,27 @@
+'use strict';
+const loginForm = document.querySelector('#login-form');
+const registerForm = document.querySelector('#register-form');
 
+// LOGIN
+loginForm.addEventListener('submit', async (evt) => {
+  evt.preventDefault();
+  const formData = loginForm.elements;
+  console.log('formData', formData);
+
+  // Hide login and show front page
+  $('#login-page').hide();
+  $('#main-pages').show();
+  loginForm.reset(); // clear forms input-values
+});
+
+// SUBMIT REGISTER FORM
+registerForm.addEventListener('submit', async (evt) => {
+  evt.preventDefault();
+  const formData = registerForm.elements;
+  console.log('formData', formData);
+
+  // Hide login and show frontpage
+  $('#login-page').hide();
+  $('#main-pages').show();
+  registerForm.reset(); // Clear forms input-values
+});
